6d410f3c3173ac0633046b8c13c1fea3c1d01c80
biancabuzila <79212200+biancabuzila@users.noreply.github.com>
2022-04-30 18:00:45 +0300

    Add files via upload

---
+++ Authentication and User Profile/App.js
@@ -0,0 +1,33 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SignIn from './Pages/Sign_In'
+import SignUp from './Pages/Sign_Up'
+import ViewProfile from './Pages/Profile/View_Profile'
+import EditProfile from './Pages/Profile/Edit_Profile'
+import DeleteAccount from './Pages/Profile/Delete_Account'
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SignIn"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "transparent"
+          },
+          headerTintColor: "#f1f1f1",
+          headerTransparent: true,
+          headerTitle: ""
+        }}>
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen options={{headerTintColor: "#f1f1f1"}} name="SignUp" component={SignUp} />
+        <Stack.Screen name="ViewProfile" component={ViewProfile} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
+        <Stack.Screen name="DeleteAccount" component={DeleteAccount} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
---
+++ Authentication and User Profile/Pages/Profile/View_Profile.js
@@ -0,0 +1,233 @@
+import { StatusBar, StyleSheet, Text, TextInput, SafeAreaView, Image, View, Pressable, ScrollView } from 'react-native';
+import React from 'react';
+
+let getName = () => { return 'Iulian Manole'; }
+let getEmail = () => { return 'iulian.manole@gmail.com'; }
+let getPhoneNumber = () => { return '0758 660 127'; }
+let getCountry = () => { return 'Romania'; }
+let getLastTransaction = () => { return '⪢Last transaction: 25/04/2022 - Loc: Mihai\'s House' }
+
+export default function ViewProfile({ navigation }) {
+
+  return (
+    <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{ flex: 1 }}
+        contentContainerStyle={{ flexGrow: 1 }}>
+      <SafeAreaView style={{backgroundColor: '#000000', minHeight: '100%'}}>
+
+        <StatusBar barStyle="default"/>
+        <View style={styles.container}>
+        
+          <View style={{flexDirection:'row', alignItems:'center', justifyContent:"flex-start", marginTop: 60, marginLeft: '10%', width: '100%'}}>
+              <Image
+                  style={styles.profileImage}
+                  source={require('../../Images/Profile_Picture.jpg')}  
+              />
+
+            <View style={{flexDirection: 'column', alignItems:'flex-start', maxWidth: '60%'}}>
+                <Text style={{fontSize: 22, color: 'grey'}}> Personal profile </Text>
+                <TextInput 
+                editable={false} 
+                textAlign={'center'} 
+                defaultValue= {''.concat('Hey ', getName(), '!')}
+                style={{
+                  alignSelf: 'flex-start',
+                  marginLeft: '5%',
+                  color: '#fff',
+                  fontSize: 28,
+                  maxWidth: '100%'}}/>
+            </View>
+          </View>
+
+          <View style={styles.card}>
+            <Text style={styles.attachLabel}> Name </Text>
+            <TextInput editable={false} defaultValue={getName()} style={styles.dataLabel}/>
+            <View style={styles.line} />
+          </View>
+
+          <View style={styles.card}>
+            <Text style={styles.attachLabel}> Email address </Text>
+            <TextInput editable={false} defaultValue={getEmail()} style={styles.dataLabel}/>
+            <View style={styles.line} />
+          </View>
+          
+          <View style={styles.card}>
+            <Text style={styles.attachLabel}> Phone Number </Text>
+            <TextInput editable={false} defaultValue={getPhoneNumber()} style={styles.dataLabel}/>
+            <View style={styles.line} />
+          </View>
+          
+          <View style={styles.card}>
+            <Text style={styles.attachLabel}> Country </Text>
+            <TextInput editable={false} defaultValue={getCountry()} style={styles.dataLabel}/>
+            <View style={styles.line} />
+          </View>
+
+          <View style={styles.paymentJournal}>
+            <Text style={{
+              fontSize: 28,
+              color: '#fff',
+              textShadowColor: '#fff',
+              textShadowRadius: 2,
+              marginLeft: 10,
+            }}> 
+              Payment Journal 
+            </Text>
+            
+            <View style={styles.line} />
+            
+            <TextInput 
+            editable={false} 
+            defaultValue={getLastTransaction()}
+            multiline={true}
+            style={{
+              fontSize: 20,
+              color: '#05CAAD',
+              height: '90%',
+              marginLeft: 10,
+            }}/>
+
+            <Image
+              source={require('../../Images/Journal_Icon.svg')}  
+              style={{
+                width: 30,
+                height: 30,
+                marginTop: 15,
+                position: 'absolute',
+                bottom: 10,
+                right: '45%',
+              }}
+            />
+
+          </View>
+
+          <Pressable style={styles.buttonEdit} onPress={() => navigation.navigate("EditProfile")}>
+            
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <Text style={styles.buttonText}> Edit Profile </Text>
+              <Image
+                  source={require('../../Images/Edit_Button.svg')}  
+                  style={{
+                    width: 20,
+                    height: 20,
+                    marginTop: 15,
+                  }}
+              />
+            </View>
+
+          </Pressable>
+
+          <Pressable style={styles.buttonLogOut} onPress={() => navigation.navigate("SignIn")}>
+
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <Text style={styles.buttonText}> Log Out </Text>
+              <Image
+                  source={require('../../Images/Logout_Icon.svg')}  
+                  style={{
+                    width: 20,
+                    height: 20,
+                    marginTop: 15
+                  }}
+              />
+            </View>
+              
+          </Pressable>
+
+        </View>
+
+      </SafeAreaView>
+     </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#0C1615",
+      alignItems: "center",
+      justifyContent: "center",
+      margin: 10,
+      borderRadius: 20,
+      borderWidth: 1,
+      borderColor: '#ffffff',
+      paddingBottom: 50,
+      },
+    line: {
+        borderBottomColor: '#05CAAD',
+        borderWidth: 2,
+        width: '100%',
+      },
+    card: {
+      backgroundColor: '#13201E', 
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10, 
+      paddingTop: 5, 
+      marginTop: 32,
+      width: '90%',
+      //opacity: 0.3,
+    },
+    profileImage: {
+        width: 68,
+        height: 68,
+        borderRadius: 400/ 2,
+        borderWidth: 3,
+        borderColor: '#05CAAD',
+        marginRight: 15,
+        marginTop: 10,
+      },
+    nameLabel: {
+        color: '#fff',
+        fontSize: 44,
+        alignSelf: 'center',
+        width: '50%',
+      },
+    attachLabel: {
+        alignSelf: 'flex-start',
+        marginLeft: '3%',
+        color: '#C0C0C0',
+        fontSize: 22,
+        //marginTop: 32,
+      },
+    dataLabel: {
+        alignSelf: 'flex-start',
+        marginLeft: '3%',
+        color: '#fff',
+        fontSize: 24,
+      },
+    buttonEdit: {
+        backgroundColor: '#04ae95',
+        width: '75%',
+        height: '7%',
+        marginTop: 50,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonLogOut: {
+        backgroundColor: '#024b40',
+        width: '50%',
+        height: '6%',
+        marginTop: 20,
+        marginBottom: 20,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+      },
+    buttonText: {
+        fontSize: 32,
+        color: 'white',
+      },
+    paymentJournal: {
+      marginTop: 30,
+      padding: '2%',
+      width: '90%',
+      height: '20%',
+      borderWidth: 3,
+      borderColor: '#04ae95',
+      borderRadius: 20,
+      backgroundColor: '#182724',
+     },
+});
+
