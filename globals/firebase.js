27b814b702745c58751089bae3fe8d2699603f44
LucianChiriac <35330198+LucianChiriac@users.noreply.github.com>
2022-05-18 19:48:30 +0300

    added bottom navigator

--- globals/firebase.js
+++ globals/firebase.js
@@ -13,7 +13,7 @@ const fireFunc = getFunctions(fireApp);
 
 if (__DEV__) {
   console.log("Switching to local Firebase instance...");
-  const origin = "192.168.38.1";
+  const origin = "192.168.0.111";
 
   //firebase.auth().useEmulator(`http://${origin}:9099/`);
   //firebase.firestore().useEmulator(origin, 8080);
